1b4e8163c817b94754d49230f96efec61a105839
Your Name <chinmayparida2003@gmail.com>
2025-09-01 17:05:57 +0530

    Your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: './',
+//   // optimizeDeps: {
+//   //   exclude: ['lucide-react'],
+//   //   base: './',
+//   // },
+// });
+
+
 export default defineConfig({
+  base: "/TYTWEB/",
   plugins: [react()],
-  base: './',
-  // optimizeDeps: {
-  //   exclude: ['lucide-react'],
-  //   base: './',
-  // },
 });
